1f097eacca6e9688f1893a689f9b3e0ce01d1b49
Vikram Belde <belvikram7@gmail.com>
2025-08-26 21:30:13 +0530

    add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import OurStory from './pages/OurStory';
-import Events from './pages/Events';
-import Donations from './pages/Donations';
-import Contact from './pages/Contact';
-import GetInvolved from './pages/GetInvolved';
-import Impact from './pages/Impact';
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Donations from "./pages/Donations";
+import Events from "./pages/Events";
+import Footer from "./components/Footer";
+import GetInvolved from "./pages/GetInvolved";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Impact from "./pages/Impact";
+import OurStory from "./pages/OurStory";
 
 function App() {
   return (
